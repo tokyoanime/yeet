b13fe8ea514580c198079d3afe13ec094b2d678c
Lwin Ye <lwin_ye@yahoo.com>
2019-09-09 15:40:37 -0700

    created sessions controller and added new, create and destroy methods

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+  before_action: require_login, only: [:new, :destroy]
+
+  def new
+    render "/signin"
+  end
+
+  def create
+    @user = User.find_user(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      render "/"
+    else
+      render json: ["Incorrect username or password"]
+    end
+  end
+
+  def destroy
+    logout
+    render "/signin"
+  end
+
+end
