0f13b77f38875e7087018c2da35da8d4bc71fca6
Lwin Ye <lwin_ye@yahoo.com>
2019-09-10 10:26:07 -0700

    added storeConfig, App & root component files, and included storeConfig and root component in entry file

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,7 +4,6 @@ import { Route, Switch, Link, HashRoute, Redirect } from 'react-router-dom';
 const App = () => (
   <div>
     <h2>Yeet!</h2>
-
     <Switch>
       <AuthRoute exact path="/login" component={} />
       <AuthRoute exact path="/signup" component={} />
