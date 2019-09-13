397d1ff35314a65d4cb6d0fd963bfbc5c0c25f41
Lwin Ye <lwin_ye@yahoo.com>
2019-09-12 17:31:27 -0700

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 User.destroy_all
 
-User.create({username: "demoUser", email: "demo@demouser.com", fname: "Demo User", lname: "Demo"});
+User.create({id: 2, username: "demoUser", email: "demo@demouser.com", fname: "Demo User", lname: "Demo"});
