ba3b582adabb031f54f4ac6d7707bce0c896984d
Lwin Ye <lwin_ye@yahoo.com>
2020-07-02 06:09:34 -0700

    Business live search controller and route

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -38,6 +38,14 @@ class Api::BusinessesController < ApplicationController
     render :index
   end
 
+  def live_search
+    query = JSON.parse(params[:query])
+    
+    @biz = Business.text_search(query)
+    @biz = @biz[0..4]
+    render :index
+  end
+
   private
   def biz_params
     params.require(:biz).permit(:biz_name, :biz_address, :biz_city, :biz_state, :biz_zipcode, :biz_lat, :biz_lng, :biz_phone, :biz_url, :biz_price_range, :biz_parking, :biz_delivery, :biz_takeout, :biz_reservations, :biz_first_cat, :biz_first_cat, :biz_second_cat, :biz_third_cat, :biz_mo_hrs, :biz_tu_hrs, :biz_we_hrs, :biz_th_hrs, :biz_fr_hrs, :biz_sa_hrs, :biz_su_hrs)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     get '/businesses/search/:query', to: 'businesses#search'
+    get '/businesses/live_search/:query', to: 'businesses#live_search'
     resources :users, only: [:show, :create, :update]
     resource :session, only: [:create, :destroy]
     resources :businesses, only: [:index, :show, :create, :update] do
