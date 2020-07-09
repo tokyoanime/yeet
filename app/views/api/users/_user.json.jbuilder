1886949f69b58985f30d4b674aa4067ee672c50d
Lwin Ye <lwin_ye@yahoo.com>
2020-07-09 01:35:56 -0700

    User's profile review section

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,2 +1,7 @@
 json.extract! user, :id, :username, :email, :fname, :lname
-json.reviews user.reviews.reverse().map{ |el| el }
+# json.reviews user.reviews.reverse().map{ |el| el }
+
+json.reviews user.reviews.each do |review|
+  json.extract! review, :id, :review_body, :review_rating, :business_id, :user_id, :created_at
+  json.biz_name review.business.biz_name
+end
--- frontend/components/user_components/profile.jsx
+++ frontend/components/user_components/profile.jsx
@@ -1,9 +1,27 @@
 import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import TopNavContainer from '../nav_components/top_nav_container';
 
 export default class Profile extends Component {
   constructor(props) {
     super(props);
+    this.renderReviews = this.renderReviews.bind(this);
+  }
+
+  renderReviews(reviews) {
+    return reviews.map((review) => {
+      return (
+        <tr key={review.id}>
+          <td>
+            <Link to={`/biz/${review.business_id}`}>{review.biz_name}</Link>
+          </td>
+          <td>{review.review_body}</td>
+          <td>
+            <Link to={`/reviews/${review.id}`}>Edit Review</Link>
+          </td>
+        </tr>
+      );
+    });
   }
 
   render() {
@@ -12,17 +30,46 @@ export default class Profile extends Component {
       return (
         <Fragment>
           <TopNavContainer />
-          <div>
+          <div className='profile-container'>
             <div>
-              <h3>Profile Overview:</h3>
-              <div>Username: {username}</div>
-              <div>Email: {email}</div>
-              <div>First name: {fname}</div>
-              <div>Last name: {lname}</div>
+              <h2>Profile Overview:</h2>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Username:</td>
+                    <td>{username}</td>
+                  </tr>
+                  <tr>
+                    <td>Email:</td>
+                    <td>{email}</td>
+                  </tr>
+                  <tr>
+                    <td>First Name:</td>
+                    <td>{fname}</td>
+                  </tr>
+                  <tr>
+                    <td>Last Name:</td>
+                    <td>{lname}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <Link to='/'>Edit Profile</Link>
             </div>
+            <br />
             <div>
               <h4>You have a total of: {reviews.length} reviews</h4>
-              {reviews.length > 0 ? <div>Reviews here</div> : null}
+              {reviews.length > 0 ? (
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Business Name</th>
+                      <th>Your Review</th>
+                      <th></th>
+                    </tr>
+                    {this.renderReviews(reviews)}
+                  </tbody>
+                </table>
+              ) : null}
             </div>
           </div>
         </Fragment>
