aa9e424d6373053f9658293512b8e424f66c52e5
Lwin Ye <lwin_ye@yahoo.com>
2020-07-29 23:42:04 -0700

    rating star css

--- frontend/components/review_components/review_edit.jsx
+++ frontend/components/review_components/review_edit.jsx
@@ -5,37 +5,38 @@ class ReviewEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: "",
-      review_body: "",
-      review_rating: "",
-    }
+      id: '',
+      review_body: '',
+      review_rating: '',
+    };
     this.handleEditReview = this.handleEditReview.bind(this);
     this.updateRating = this.updateRating.bind(this);
   }
 
   componentDidMount() {
     this.props.clearErrors();
-    this.props.getReview(this.props.match.params.reviewId)
-      .then( () => {
-        if (this.state.review_body === "") {
-          this.setState({
-            id: this.props.review.id,
-            review_body: this.props.review.review_body,
-            review_rating: this.props.review.review_rating
-          });
-          this.genStar();
-        }
-      });
+    this.props.getReview(this.props.match.params.reviewId).then(() => {
+      if (this.state.review_body === '') {
+        this.setState({
+          id: this.props.review.id,
+          review_body: this.props.review.review_body,
+          review_rating: this.props.review.review_rating,
+        });
+        this.genStar();
+      }
+    });
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.match.params.reviewId !== this.props.match.params.reviewId) {
-      this.props.getReview(this.props.match.params.reviewId)
-        .then(() => {
-          this.props.clearErrors();
-          this.setState({ review_body: this.props.review.review_body, review_rating: this.props.review.review_rating });
-          this.genStar();
+      this.props.getReview(this.props.match.params.reviewId).then(() => {
+        this.props.clearErrors();
+        this.setState({
+          review_body: this.props.review.review_body,
+          review_rating: this.props.review.review_rating,
         });
+        this.genStar();
+      });
     }
   }
 
@@ -43,20 +44,20 @@ class ReviewEdit extends React.Component {
     const currentRating = rating || this.state.review_rating;
 
     for (let i = 1; i <= currentRating; i++) {
-      const tempStar = document.getElementById(`star-${i}`)
-      tempStar.style.color = "#FFA500";
+      const tempStar = document.getElementById(`star-${i}`);
+      tempStar.style.color = '#d43434';
     }
 
-    for (let j = (currentRating + 1); j <= 5; j++) {
-      const tempStar = document.getElementById(`star-${j}`)
-      tempStar.style.color = "#DDDDDD";
+    for (let j = currentRating + 1; j <= 5; j++) {
+      const tempStar = document.getElementById(`star-${j}`);
+      tempStar.style.color = '#DDDDDD';
     }
   }
 
   updateField(field) {
     return (e) => {
-      this.setState({ [field]: e.currentTarget.value })
-    }
+      this.setState({ [field]: e.currentTarget.value });
+    };
   }
 
   updateRating(val) {
@@ -66,54 +67,95 @@ class ReviewEdit extends React.Component {
 
   handleEditReview(e) {
     e.preventDefault();
-    this.props.updateReview(this.state)
-      .then( () => this.props.history.push(`/biz/${this.props.review.business_id}`));
+    this.props
+      .updateReview(this.state)
+      .then(() =>
+        this.props.history.push(`/biz/${this.props.review.business_id}`)
+      );
   }
 
   render() {
     const { review } = this.props;
 
     const errors = this.props.reviewErr.map((err) => {
-      return (
-        <div className="review-error">{err}</div>
-      )
+      return <div className='review-error'>{err}</div>;
     });
 
     if (!review) {
-      return null
-    };
+      return null;
+    }
 
-    const requireLogin = (
-      <div>Log in to update review.</div>
-    )
+    const requireLogin = <div>Log in to update review.</div>;
 
     const reviewForm = (
-      <div className="review-edit-form-container">
-        <div className="review-edit-form">
-          {(errors.length > 0) ? errors : ""}
-          <div className="review-biz-name">{review.biz_name}</div>
+      <div className='review-edit-form-container'>
+        <div className='review-edit-form'>
+          {errors.length > 0 ? errors : ''}
+          <div className='review-biz-name'>{review.biz_name}</div>
           <form onSubmit={this.handleEditReview}>
-            <div className="star-rating-container">
-              <i className="material-icons" id="star-1" onClick={() => this.updateRating(1)}>star</i>
-              <i className="material-icons" id="star-2" onClick={() => this.updateRating(2)}>star</i>
-              <i className="material-icons" id="star-3" onClick={() => this.updateRating(3)}>star</i>
-              <i className="material-icons" id="star-4" onClick={() => this.updateRating(4)}>star</i>
-              <i className="material-icons" id="star-5" onClick={() => this.updateRating(5)}>star</i>
+            <div className='star-rating-container'>
+              <i
+                className='material-icons'
+                id='star-1'
+                onClick={() => this.updateRating(1)}
+              >
+                star
+              </i>
+              <i
+                className='material-icons'
+                id='star-2'
+                onClick={() => this.updateRating(2)}
+              >
+                star
+              </i>
+              <i
+                className='material-icons'
+                id='star-3'
+                onClick={() => this.updateRating(3)}
+              >
+                star
+              </i>
+              <i
+                className='material-icons'
+                id='star-4'
+                onClick={() => this.updateRating(4)}
+              >
+                star
+              </i>
+              <i
+                className='material-icons'
+                id='star-5'
+                onClick={() => this.updateRating(5)}
+              >
+                star
+              </i>
             </div>
-            <textarea placeholder="Write your review here." onChange={this.updateField('review_body')} defaultValue={review.review_body}></textarea>
-            {((this.props.currentUser) && (this.props.currentUser.id === review.user_id)) ? (
-              <input className="submit-bttn" type="submit" value="Update Review" />) : requireLogin}
+            <textarea
+              placeholder='Write your review here.'
+              onChange={this.updateField('review_body')}
+              defaultValue={review.review_body}
+            ></textarea>
+            {this.props.currentUser &&
+            this.props.currentUser.id === review.user_id ? (
+              <input
+                className='submit-bttn'
+                type='submit'
+                value='Update Review'
+              />
+            ) : (
+              requireLogin
+            )}
           </form>
         </div>
       </div>
-    )
+    );
 
     return (
       <div>
         <TopNavContainer />
         {reviewForm}
       </div>
-    )
+    );
   }
 }
 
--- frontend/components/review_components/review_form.jsx
+++ frontend/components/review_components/review_form.jsx
@@ -8,11 +8,10 @@ class ReviewForm extends React.Component {
     this.updateField = this.updateField.bind(this);
     this.updateRating = this.updateRating.bind(this);
     this.state = {
-      review_body: "",
-      review_rating: "",
-      review_posted: false
-    }
-
+      review_body: '',
+      review_rating: '',
+      review_posted: false,
+    };
   }
 
   componentDidMount() {
@@ -21,8 +20,8 @@ class ReviewForm extends React.Component {
 
   updateField(field) {
     return (e) => {
-      this.setState({ [field]: e.currentTarget.value })
-    }
+      this.setState({ [field]: e.currentTarget.value });
+    };
   }
 
   updateRating(val) {
@@ -33,30 +32,29 @@ class ReviewForm extends React.Component {
   handlePostReview(e) {
     e.preventDefault();
     const tempReview = {
-      'review_body': this.state.review_body,
-      'review_rating': this.state.review_rating,
-      'user_id': this.props.currentUser.id,
-      'business_id': this.props.match.params.bizId
+      review_body: this.state.review_body,
+      review_rating: this.state.review_rating,
+      user_id: this.props.currentUser.id,
+      business_id: this.props.match.params.bizId,
     };
 
-    this.props.createReview(tempReview)
-      .then( () => {
-        this.setState({ review_posted: true});
-        this.props.fetchReviews(tempReview.business_id);
-      })
+    this.props.createReview(tempReview).then(() => {
+      this.setState({ review_posted: true });
+      this.props.fetchReviews(tempReview.business_id);
+    });
   }
 
   genStar(rating) {
     const currentRating = rating || this.state.review_rating;
 
     for (let i = 1; i <= currentRating; i++) {
-      const tempStar = document.getElementById(`star-${i}`)
-      tempStar.style.color = "#FFA500";
+      const tempStar = document.getElementById(`star-${i}`);
+      tempStar.style.color = '#d43434';
     }
 
-    for (let j = (currentRating + 1); j <= 5; j++) {
-      const tempStar = document.getElementById(`star-${j}`)
-      tempStar.style.color = "#DDDDDD";
+    for (let j = currentRating + 1; j <= 5; j++) {
+      const tempStar = document.getElementById(`star-${j}`);
+      tempStar.style.color = '#DDDDDD';
     }
   }
 
@@ -65,48 +63,83 @@ class ReviewForm extends React.Component {
     if (this.props.reviewErr) {
       errors = this.props.reviewErr.map((err, i) => {
         return (
-          <div className="review-error" key={`err-${i}`}>{err}</div>
-        )
+          <div className='review-error' key={`err-${i}`}>
+            {err}
+          </div>
+        );
       });
     }
-    
 
     const reviewForm = (
       <form onSubmit={this.handlePostReview}>
-        <div className="star-rating-container">
-          <i className="material-icons" id="star-1" onClick={() => this.updateRating(1)}>star</i>
-          <i className="material-icons" id="star-2" onClick={() => this.updateRating(2)}>star</i>
-          <i className="material-icons" id="star-3" onClick={() => this.updateRating(3)}>star</i>
-          <i className="material-icons" id="star-4" onClick={() => this.updateRating(4)}>star</i>
-          <i className="material-icons" id="star-5" onClick={() => this.updateRating(5)}>star</i>
+        <div className='star-rating-container'>
+          <i
+            className='material-icons'
+            id='star-1'
+            onClick={() => this.updateRating(1)}
+          >
+            star
+          </i>
+          <i
+            className='material-icons'
+            id='star-2'
+            onClick={() => this.updateRating(2)}
+          >
+            star
+          </i>
+          <i
+            className='material-icons'
+            id='star-3'
+            onClick={() => this.updateRating(3)}
+          >
+            star
+          </i>
+          <i
+            className='material-icons'
+            id='star-4'
+            onClick={() => this.updateRating(4)}
+          >
+            star
+          </i>
+          <i
+            className='material-icons'
+            id='star-5'
+            onClick={() => this.updateRating(5)}
+          >
+            star
+          </i>
         </div>
-        <textarea rows="10" placeholder="Write your review here." onChange={this.updateField('review_body')} defaultValue={this.state.review_body}></textarea>
-        <input type="submit" value="Post Review" />
+        <textarea
+          rows='10'
+          placeholder='Write your review here.'
+          onChange={this.updateField('review_body')}
+          defaultValue={this.state.review_body}
+        ></textarea>
+        <input type='submit' value='Post Review' />
       </form>
-    )
+    );
 
     const requestLogin = (
-      <div>Please <Link to='/login'>login</Link> to post/edit review.</div>
-    )
-    
+      <div>
+        Please <Link to='/login'>login</Link> to post/edit review.
+      </div>
+    );
+
     if (this.state.review_posted) {
-      return(
-        <div className="biz-review-form">
-          Your review has been posted.
-        </div>
-      )
+      return (
+        <div className='biz-review-form'>Your review has been posted.</div>
+      );
     } else {
       return (
         <div>
-          {(errors.length > 0) ? errors : ""}
-          <div className="biz-review-form">
-            {(this.props.currentUser) ? reviewForm : requestLogin}
+          {errors.length > 0 ? errors : ''}
+          <div className='biz-review-form'>
+            {this.props.currentUser ? reviewForm : requestLogin}
           </div>
         </div>
-      )
+      );
     }
   }
-  
 }
 
 export default ReviewForm;
